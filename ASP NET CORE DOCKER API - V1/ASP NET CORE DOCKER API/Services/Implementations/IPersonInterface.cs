00796ee6043a52f36b5588b18682f8c5ee02d166
Eduardo Marques dos Santos <mixa_mix@hotmail.com>
2020-11-11 01:23:34 -0300

    Criando Injeção de dependência

---
+++ ASP NET CORE DOCKER API - V1/ASP NET CORE DOCKER API/Services/Implementations/IPersonInterface.cs
@@ -0,0 +1,14 @@
+﻿using ASP_NET_CORE_DOCKER_API.Model;
+using System.Collections.Generic;
+
+namespace ASP_NET_CORE_DOCKER_API.Services.Implementations
+{
+    public interface IPersonInterface
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
